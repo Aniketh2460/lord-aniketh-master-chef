649a51da2d38396b75f9ed5dfc14b1baaec8281b
Aniketh N <anikethn@Mac.lan>
2026-06-18 17:05:53 +0530

    Added Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,26 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+     <body className="min-h-full flex flex-col">
+
+  <Script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-2JZBK2FNQG"
+  />
+
+  <Script id="google-analytics">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-2JZBK2FNQG');
+    `}
+  </Script>
+
+  {children}
+
+</body>
     </html>
   );
 }
